e89f2dbc94b35d7d8f0449e253e745d31d3c6731
André Cancella de Paula <siscancella@gmail.com>
2026-01-16 10:56:10 -0300

    title name

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -16,14 +16,26 @@ export default function RootLayout() {
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         {/* CONFIGURAÇÃO ESPECÍFICA DA HOME: Remove o header apenas aqui */}
-        <Stack.Screen name="index" options={{ title: 'Login' }} />
-        <Stack.Screen name="home" options={{ title: 'Início' }} />
+        <Stack.Screen 
+        name="index" 
+        options={{ 
+          title: 'Acessar Conta', // Título que aparece no navegador
+          headerShown: false      // Se quiser esconder a barra de cima no Login
+        }} 
+      />
+      <Stack.Screen 
+        name="home" 
+        options={{ 
+          title: 'Minha Home', 
+          headerShown: true 
+        }} 
+      />
 
         {/* O RESTO CONTINUA COM O PADRÃO: (tabs) e modal */}
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
       </Stack>
-
+      
       {/* "auto" ou "light" dependendo da sua preferência visual */}
       <StatusBar style="light" />
     </ThemeProvider>
